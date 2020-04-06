bb13130f5e9911ac4cdf9568f502b40442aa091f
Léa DUKAEZ <lea.dukaez@gmail.com>
2020-04-06 14:52:59 +0200

    Initial Commit

---
+++ Split The Bill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Split The Bill
+//
+//  Created by Léa on 06/04/2020.
+//  Copyright © 2020 Lea Dukaez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
